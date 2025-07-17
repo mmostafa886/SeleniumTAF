9862182100f9a95fca55189281d0e702842d548c
mmostafa886 <mmostafa886@gmail.com>
2025-07-17 23:45:15 +0200

    Initial commit

--- src/main/java/com/taf/utils/logs/LogManager.java
+++ src/main/java/com/taf/utils/logs/LogManager.java
@@ -9,22 +9,22 @@ public class LogManager {
     }
 
     public static void info(String ... message) {
-        logger().info(String.join("", message));
+        logger().info(String.join(" ", message));
     }
 
     public static void error(String ... message) {
-        logger().error(String.join("", message));
+        logger().error(String.join(" ", message));
     }
 
     public static void warn(String ... message) {
-        logger().warn(String.join("", message));
+        logger().warn(String.join(" ", message));
     }
 
     public static void debug(String ... message) {
-        logger().debug(String.join("", message));
+        logger().debug(String.join(" ", message));
     }
 
     public static void fatal(String ... message) {
-        logger().fatal(String.join("", message));
+        logger().fatal(String.join(" ", message));
     }
 }
