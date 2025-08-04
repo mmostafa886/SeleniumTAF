dc1e3fe863e4d354dd68b59533071caacfc16ef4
mmostafa886 <mmostafa886@gmail.com>
2025-08-04 23:42:37 +0200

    -Enable Using groups for mvn execution commands -Improve the LogsManager to log the line number causes the log line -Use groups in Tests

---
+++ src/main/java/com/taf/utils/Groups.java
@@ -0,0 +1,12 @@
+package com.taf.utils;
+
+public class ExecutionGroups {
+    public static final String SMOKE = "Smoke";
+    public static final String REGRESSION = "Regression";
+    public static final String LOGIN = "Login";
+    public static final String CART = "Cart";
+    public static final String PRODUCT = "Product";
+
+    private ExecutionGroups() {
+    }
+}
